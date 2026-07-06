72370748d87e0b959dfb7434db98ba1eeefb01fc
nattan junior <fnatanieljunior@gmail.com>
2026-07-06 15:15:25 -0300

    Add video processing with FFmpeg and update server to stream processed video

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { createServer } from "node:http";
 import { createReadStream } from "node:fs";
+import { spawn } from "node:child_process";
+
 
 createServer((req, res) => {
   const headers = {
@@ -11,8 +13,38 @@ createServer((req, res) => {
     res.end();
     return;
   }
-  createReadStream("./assets/video.mp4")
-    .pipe(res);
+
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4',
+  });
+
+  const ffmpegProcess = spawn('ffmpeg', [
+    '-i', 'pipe:0',
+    '-f', 'mp4',
+    '-vcodec', 'h264',
+    '-acodec', 'aac',
+    '-movflags', 'frag_keyframe+empty_moov+default_base_moof',
+    '-b:v', '1500k',
+    '-maxrate', '1500k',
+    '-bufsize', '1000k',
+    '-f', 'mp4',
+    'pipe:1'
+  ], {
+    stdio: ['pipe', 'pipe', 'pipe']
+  });
+
+  createReadStream("./assets/video-ready.mp4")
+    .pipe(ffmpegProcess.stdin);
+
+  ffmpegProcess.stderr.on('data', (data) => console.log(`FFmpeg stderr: ${data}`));
+  ffmpegProcess.stdout.pipe(res);
+
+  req.on('close', () => {
+    ffmpegProcess.stdout.destroy();
+    ffmpegProcess.stdin.destroy();
+    console.log("disconnected", ffmpegProcess.kill());
+  });
+
 }).listen(3000, () => {
   console.log("Server running at http://localhost:3000/");
 }); 
